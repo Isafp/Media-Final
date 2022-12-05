bf495e66153f020e07e12057393b3daba8e90bb2
Isabela Ferreira Paranaíba <isafparanaiba@gmail.com>
2022-12-05 19:07:24 -0300

    Update calculo.py

--- calculo.py
+++ calculo.py
@@ -3,17 +3,5 @@ import streamlit as st
 
 y=int(input("Digite sua nota MI: "))
 f = 120-y
-
-  def main():
-  st.title("Média Final")
-  st.write("Calculadora simples")
-    
-  operation = st.selectbox("Selecione uma operação", ["Calcular"])
- if operation == "Calcular":
-
-    print("Você precisa tirar no mínimo "+ str(f))
-    
-  else:
-    st.write("0000")
-if __name__ == '__main__':
-  main()
+print("Você precisa tirar no mínimo "+ str(f))
+  
